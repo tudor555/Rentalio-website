9d51eef8e643757f6a8f8da9278688bc5e2679f2
Tudor_Bumb <bumbtudor03@gmail.com>
2024-12-26 16:31:10 +0200

    Finish implement navbar section

--- frontend/src/rental-ui/src/app/components/navbar/navbar.component.ts
+++ frontend/src/rental-ui/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule, CommonModule],
+  standalone: true, // Ensure standalone is true
+  imports: [RouterModule, CommonModule], // Import CommonModule here
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   isMobileMenuOpen: boolean = false;
+  currentUrl: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl = event.urlAfterRedirects;
+      }
+    });
+  }
 
   toggleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  isActive(route: string): boolean {
+    return this.currentUrl === route;
   }
 }
