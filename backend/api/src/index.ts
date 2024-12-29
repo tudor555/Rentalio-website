323b984c7314c1a366acee1a4166a1b5e19be542
Tudor_Bumb <bumbtudor03@gmail.com>
2024-12-29 19:52:19 +0200

    Start implement authentication on api, add user schema, add service for database

---
+++ backend/api/src/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import { error } from "console";
+
+const envPath = path.resolve(
+  __dirname,
+  "../../../env/backend_env/api_env/api.env"
+);
+dotenv.config({ path: envPath });
+
+const port = process.env.API_PORT || 3000;
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
+
+const MONGO_URL = process.env.MONGO_URL || "";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
