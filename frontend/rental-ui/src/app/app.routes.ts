e5e7c02e8dbb4c3b4fb6ad09015621d1db8ae173
Tudor_Bumb <bumbtudor03@gmail.com>
2025-03-13 20:37:08 +0200

    Add routes for authentication

--- frontend/rental-ui/src/app/app.routes.ts
+++ frontend/rental-ui/src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { FlightsComponent } from './pages/flights/flights.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { AdminComponent } from './pages/admin/admin.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { SignupComponent } from './pages/auth/signup/signup.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -17,5 +19,7 @@ export const routes: Routes = [
   { path: 'contact-us', component: ContactUsComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: '**', component: NotFoundComponent }, // Wildcard route for 404
 ];
