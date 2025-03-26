6c6e8fadab4bbb2ccbb29188b77900a9b0e5d9a4
Tudor_Bumb <bumbtudor03@gmail.com>
2025-03-26 10:42:32 +0200

    Homapege ui done, buttons working

--- frontend/rental-ui/src/app/pages/home/home.component.ts
+++ frontend/rental-ui/src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  @ViewChild('choose') chooseDiv!: ElementRef;
 
+  // Function to handle the scroll action
+  scrollToChoose() {
+    this.chooseDiv.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
 }
