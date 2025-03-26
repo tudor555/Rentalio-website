d36e4eb5e2690e84eb8a5b614491a2afc031667e
Tudor_Bumb <bumbtudor03@gmail.com>
2025-03-26 10:51:23 +0200

    homapage design and button works

---
+++ frontend/rental-ui/src/app/pages/admin/admin-users/admin-users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-users',
+  imports: [],
+  templateUrl: './admin-users.component.html',
+  styleUrl: './admin-users.component.scss'
+})
+export class AdminUsersComponent {
+
+}
---
+++ frontend/rental-ui/src/app/pages/rentals/rental/rental.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rental',
+  imports: [],
+  templateUrl: './rental.component.html',
+  styleUrl: './rental.component.scss'
+})
+export class RentalComponent {
+
+}
