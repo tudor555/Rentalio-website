a36f58eb8b68b6a0298b5e83bbc71457ea9f5806
Tudor_Bumb <bumbtudor03@gmail.com>
2025-03-30 17:28:46 +0300

    Changes hover buttons and if button click render to homepage

--- frontend/rental-ui/src/app/pages/auth/login/login.component.ts
+++ frontend/rental-ui/src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  login() {
+    // Implement your login logic here
+    this.router.navigate(['/home']); // Redirect to home page after login
+  }
 }
--- frontend/rental-ui/src/app/pages/auth/signup/signup.component.ts
+++ frontend/rental-ui/src/app/pages/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.scss',
 })
 export class SignupComponent {
+  constructor(private route: Router) {}
+
   onPhoneInput(event: any) {
     const input = event.target;
     // Replace non-digit characters with empty string
     input.value = input.value.replace(/\D/g, '');
+  }
+
+  signup() {
+    this.route.navigate(['/home']); // Redirect to home page after signup
   }
 }
