f555274114ee6a91bffc37a53293b26082969fd2
Tudor_Bumb <bumbtudor03@gmail.com>
2025-04-06 16:33:19 +0300

    Made login function with call to api

--- frontend/rental-ui/src/app/pages/auth/login/login.component.ts
+++ frontend/rental-ui/src/app/pages/auth/login/login.component.ts
@@ -1,17 +1,45 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ApiService } from '../../../services/api.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  email = '';
+  password = '';
+  error: string | null = null;
 
-  login() {
-    // Implement your login logic here
-    this.router.navigate(['/home']); // Redirect to home page after login
+  constructor(private api: ApiService, private router: Router) {}
+
+
+  login(form: NgForm) {
+    if (form.invalid) {
+      // Mark all as touched to show validation messages
+      form.control.markAllAsTouched();
+      return;
+    }
+
+    const credentials = {
+      email: this.email,
+      password: this.password,
+    };
+
+    this.api.post<{ token: string }>('auth/login', credentials).subscribe({
+      next: (res) => {
+        localStorage.setItem('token', res.token); // Store token
+        // TODO: return the user to the page that was before login action
+        this.router.navigate(['/home']);        // Redirect to profile
+      },
+      error: (err) => {
+        this.error = err?.error?.error || 'Login failed';
+        console.error('Login error:', err);
+      },
+    });
   }
 }
