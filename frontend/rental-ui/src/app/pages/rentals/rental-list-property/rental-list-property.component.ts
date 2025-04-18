7ab80bc918fdeaaef64bc675f845b3712233298f
Tudor_Bumb <bumbtudor03@gmail.com>
2025-04-18 22:49:33 +0300

    Add rantal-list omponent

---
+++ frontend/rental-ui/src/app/pages/rentals/rental-list-property/rental-list-property.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rental-list-property',
+  imports: [],
+  templateUrl: './rental-list-property.component.html',
+  styleUrl: './rental-list-property.component.scss'
+})
+export class RentalListPropertyComponent {
+
+}
