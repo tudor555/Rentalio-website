0b8de40c60573652feff06ee505fb5adddf94e4a
Tudor_Bumb <bumbtudor03@gmail.com>
2025-04-21 17:17:09 +0300

    Update how can read the cookie

--- frontend/rental-ui/src/app/services/cookie.service.ts
+++ frontend/rental-ui/src/app/services/cookie.service.ts
@@ -1,18 +1,20 @@
 export class CookieUtil {
-    static setCookie(name: string, value: string, days = 7) {
-      const expires = new Date(Date.now() + days * 864e5).toUTCString();
-      document.cookie = `${name}=${encodeURIComponent(value)}; expires=${expires}; path=/`;
-    }
-  
-    static getCookie(name: string) {
-      return document.cookie.split('; ').reduce((r, v) => {
-        const parts = v.split('=');
-        return parts[0] === name ? decodeURIComponent(parts[1]) : r;
-      }, '');
-    }
-  
-    static deleteCookie(name: string) {
-      this.setCookie(name, '', -1);
-    }
+  static setCookie(name: string, value: string, days = 7) {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = `${name}=${encodeURIComponent(
+      value
+    )}; expires=${expires}; path=/`;
   }
-  
+
+  static getCookie(name: string): string {
+    if (typeof document === 'undefined') return '';
+    return document.cookie.split('; ').reduce((r, v) => {
+      const parts = v.split('=');
+      return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+    }, '');
+  }
+
+  static deleteCookie(name: string) {
+    this.setCookie(name, '', -1);
+  }
+}
