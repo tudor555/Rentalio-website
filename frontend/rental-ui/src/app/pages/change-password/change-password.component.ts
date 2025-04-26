96f572deb408f17a0634fd480a303c9c155157cf
Tudor_Bumb <bumbtudor03@gmail.com>
2025-04-26 12:28:45 +0300

    Add component for change password

---
+++ frontend/rental-ui/src/app/pages/change-password/change-password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password',
+  imports: [],
+  templateUrl: './change-password.component.html',
+  styleUrl: './change-password.component.scss'
+})
+export class ChangePasswordComponent {
+
+}
