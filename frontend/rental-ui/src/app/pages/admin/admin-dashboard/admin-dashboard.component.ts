b1999f074fcea475e5be80fce292e6bbc9557a7d
Tudor_Bumb <bumbtudor03@gmail.com>
2025-06-22 19:06:07 +0300

    Improve ui, take data from API

--- frontend/rental-ui/src/app/pages/admin/admin-dashboard/admin-dashboard.component.ts
+++ frontend/rental-ui/src/app/pages/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ApiService } from '../../../services/api.service';
+import { NgClass, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [RouterModule],
+  imports: [RouterModule, NgFor, NgClass],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
@@ -14,16 +15,24 @@ export class AdminDashboardComponent implements OnInit {
   totalListings = 0;
   totalReservations = 0;
 
+  users: any[] = [];
+  listings: any[] = [];
+  reservations: any[] = [];
+
   ngOnInit(): void {
     this.apiService.get<any[]>('users', true).subscribe((users) => {
+      this.users = users;
       this.totalUsers = users.length;
     });
 
-    this.apiService.get<any[]>('listings').subscribe((listings) => {
-      this.totalListings = listings.length;
+    this.apiService.get<any>('listings/search?sort=createdAt_desc').subscribe((listings) => {
+      this.listings = listings.data || [];
+      console.log(listings);
+      this.totalListings = listings.total;
     });
 
     this.apiService.get<any[]>('reservations').subscribe((reservations) => {
+      this.reservations = reservations;
       this.totalReservations = reservations.length;
     });
   }
