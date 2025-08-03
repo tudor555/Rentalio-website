7fca565023ff09613e10536e297867e5c703506e
Tudor_Bumb <bumbtudor03@gmail.com>
2025-08-03 15:00:24 +0300

    Add endpoint for search users

--- backend/api/src/routes/users.ts
+++ backend/api/src/routes/users.ts
@@ -5,6 +5,7 @@ import {
   deleteUser,
   updateUser,
   updatePassword,
+  searchUsers,
 } from "../controllers/users";
 import {
   isAuthenticated,
@@ -12,6 +13,7 @@ import {
   isOwnerOrAdmin,
   checkRoleChange,
   validateObjectId,
+  isAdmin,
 } from "../middlewares";
 
 export default (router: express.Router) => {
@@ -23,6 +25,29 @@ export default (router: express.Router) => {
       next(error);
     }
   });
+
+  // GET users with certain parameters
+  // Example usage of /users/search with query parameters:
+  // - ?username=string
+  // - ?email=string
+  // - ?role=string
+  // - ?pageSize=number
+  // - ?page=number
+  // - ?sort=price_asc (options: createdAt_asc, createdAt_desc, username_asc, username_desc)
+  // All query parameters are optional and can be combined
+  // Returns filtered and sorted listings based on query
+  router.get(
+    "/users/search",
+    isAuthenticated,
+    isAdmin,
+    async (req, res, next) => {
+      try {
+        await searchUsers(req, res);
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
 
   // GET user by id
   router.get(
