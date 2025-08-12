9b9aec869b954d4ebc527c2550b87d1d7d011b86
Tudor_Bumb <bumbtudor03@gmail.com>
2025-08-12 20:25:38 +0300

    Add base functionality for manage reservations

--- frontend/rental-ui/src/app/pages/admin/admin-reservations/admin-reservations.component.ts
+++ frontend/rental-ui/src/app/pages/admin/admin-reservations/admin-reservations.component.ts
@@ -1,11 +1,50 @@
+import { DatePipe, NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { ApiService } from '../../../services/api.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-reservations',
-  imports: [],
+  imports: [RouterModule, DatePipe, NgClass, NgFor],
   templateUrl: './admin-reservations.component.html',
-  styleUrl: './admin-reservations.component.scss'
+  styleUrl: './admin-reservations.component.scss',
 })
 export class AdminReservationsComponent {
+  reservations: any;
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.fetchReservations();
+  }
+
+  fetchReservations() {
+    this.apiService.get<any[]>('reservations').subscribe((reservations) => {
+      const fetchRentals = reservations.map(async (res: any) => {
+        try {
+          const listing = await firstValueFrom(
+            this.apiService.get<any>(`listings/${res.listingId}`, true)
+          );
+          return {
+            ...res,
+            listingTitle: listing.title,
+          };
+        } catch {
+          return {
+            ...res,
+            listingTitle: res.listingId,
+          };
+        }
+      });
+
+      Promise.all(fetchRentals).then((results) => {
+        this.reservations = results;
+      });
+    });
+  }
+
+  searchReservation() {}
+
+  openCancelModal(test: any) {}
 }
